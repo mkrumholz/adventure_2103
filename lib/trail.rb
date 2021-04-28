b948de4073c44b5208837b096a0b27b3d519b25e
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-28 09:06:41 -0600

    Build Trail#init

---
+++ lib/trail.rb
@@ -0,0 +1,11 @@
+class Trail
+  attr_reader :name,
+              :length,
+              :level
+              
+  def initialize(details)
+    @name = details[:name]
+    @length = details[:length].to_f
+    @level = details[:level]
+  end
+end
