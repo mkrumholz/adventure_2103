fb18f718258c0ef53a81b7dbf519e493c721ef01
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-28 09:22:37 -0600

    Write test for Hiker#pack

--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -20,7 +20,22 @@ describe Hiker do
     it 'starts with no snacks' do
       hiker = Hiker.new('Dora', :moderate)
 
-      expect(hiker.snacks).to eq({})      
+      expect(hiker.snacks).to eq({})
+    end
+  end
+
+  describe '#pack' do
+    it 'adds snack and quantity to snacks hash' do
+      hiker = Hiker.new('Dora', :moderate)
+
+      hiker.pack('water', 1)
+      hiker.pack('trail mix', 3)
+
+      expected = {
+        'water' => 1,
+        'trail mix' => 3
+      }
+      expect(hiker.snacks).to eq expected
     end
   end
 end
