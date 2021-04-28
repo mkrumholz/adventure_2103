16eb43bdf5f3fe346579f3aff8a7f9a9683984dd
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-28 09:45:00 -0600

    Write test for Hiker#possible_trails

--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -57,4 +57,62 @@ describe Hiker do
       expect(hiker.parks_visited).to eq [park_1, park_2]
     end
   end
+
+  describe '#possible_trails' do
+    it 'returns trails from parks_visited matching hiker experience' do
+      hiker = Hiker.new('Dora', :moderate)
+      details_1 = {
+        name: 'Grand Wash',
+        length: '2.2 miles',
+        level: :easy
+      }
+      trail_1 = Trail.new(details_1)
+      details_2 = {
+        name: 'Cohab Canyon',
+        length: '1.7 miles',
+        level: :moderate
+      }
+      trail_2 = Trail.new(details_2)
+      details_3 = {
+        name: 'Chimney Rock Loop',
+        length: '3.6 miles',
+        level: :strenuous
+      }
+      trail_3 = Trail.new(details_3)
+      details_4 = {
+        name: 'Queen\'s/Navajo Loop',
+        length: '2.9 miles',
+        level: :moderate
+      }
+      trail_4 = Trail.new(details_4)
+      details_5 = {
+        name: 'Rim Trail',
+        length: '11 miles',
+        level: :easy
+      }
+      trail_5 = Trail.new(details_5)
+      details_6 = {
+        name: 'Tower Bridge',
+        length: '3 miles',
+        level: :moderate
+      }
+      trail_6 = Trail.new(details_6)
+      park1 = Park.new('Capitol Reef')
+      park2 = Park.new('Bryce Canyon')
+
+
+      park1.add_trail(trail_1)
+      park1.add_trail(trail_2)
+      park1.add_trail(trail_3)
+      park2.add_trail(trail_4)
+      park2.add_trail(trail_5)
+      park2.add_trail(trail_6)
+
+      hiker.visit(park1)
+      hiker.visit(park2)
+
+      expected = [trail_2, trail_4, trail_6]
+      expect(hiker.possible_trails).to eq expected
+    end
+  end
 end
