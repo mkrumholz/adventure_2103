b231c42783d07e654f3bafcb62533380bb571299
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-28 09:59:51 -0600

    Write test for Park#trails_by_level

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -135,4 +135,67 @@ describe Park do
       expect(park.trails_for_experience_level(:moderate)).to eq [trail_2]
     end
   end
+
+  describe '#trails_by_level' do
+    it 'returns levels (keys) and lists of trail names (values)' do
+      details_1 = {
+        name: 'Grand Wash',
+        length: '2.2 miles',
+        level: :easy
+      }
+      trail_1 = Trail.new(details_1)
+      details_2 = {
+        name: 'Cohab Canyon',
+        length: '1.7 miles',
+        level: :moderate
+      }
+      trail_2 = Trail.new(details_2)
+      details_3 = {
+        name: 'Chimney Rock Loop',
+        length: '3.6 miles',
+        level: :strenuous
+      }
+      trail_3 = Trail.new(details_3)
+      details_4 = {
+        name: 'Queen\'s/Navajo Loop',
+        length: '2.9 miles',
+        level: :moderate
+      }
+      trail_4 = Trail.new(details_4)
+      details_5 = {
+        name: 'Rim Trail',
+        length: '11 miles',
+        level: :easy
+      }
+      trail_5 = Trail.new(details_5)
+      details_6 = {
+        name: 'Tower Bridge',
+        length: '3 miles',
+        level: :moderate
+      }
+      trail_6 = Trail.new(details_6)
+      park1 = Park.new('Capitol Reef')
+      park2 = Park.new('Bryce Canyon')
+
+
+      park1.add_trail(trail_1)
+      park1.add_trail(trail_2)
+      park1.add_trail(trail_3)
+      park2.add_trail(trail_4)
+      park2.add_trail(trail_5)
+      park2.add_trail(trail_6)
+
+      expected_1 = {
+         :easy => ["Grand Wash"],
+         :moderate => ["Cohab Canyon"],
+         :strenuous => ["Chimney Rock Loop"]
+       }
+       expected_2 = {
+         :moderate => ["Queen's/Navajo Loop", "Tower Bridge"],
+         :easy => ["Rim Trail"]
+       }
+       expect(park1.trails_by_level).to eq expected_1
+       expect(park2.trails_by_level).to eq expected_2
+    end
+  end
 end
